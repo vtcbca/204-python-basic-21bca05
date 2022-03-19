072be1b6c9db910a098f1402c56a016facf448fc
21bca05 <96609493+21bca05@users.noreply.github.com>
2022-03-19 10:43:34 +0530

    solution1.py uploded...

---
+++ solution1.py.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter value for number1:"))
+number2=int(input("Enter value for number2:"))
+sum=number1+number2
+print("sum of{} and {}".format(number1,number2,sum))
+
+
+    
+    
